73db87fe788d54c92f41df70d7d7b56ebc7e37e8
klapoon <35404451+klapoon@users.noreply.github.com>
2021-02-09 19:27:40 +0100

    Add comments to code, correct border styling of sudoku game, and centre input fields for game

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let sudoku_grid = document.getElementById("sudoku-grid");
 
-for (let i = 0; i < 81; i++) {
-    sudoku_grid.innerHTML += '<div><input class="sudoku-element" type="text" maxlength="1" minlength="1"></div>'
+for (let i = 1; i < 82; i++) {
+    // Add input fields into sudoku game
+    sudoku_grid.innerHTML += '<div class="sudoku-element" id="sudoku-element-' + i + '"><input type="text" maxlength="1" minlength="1"></div>'
+    // Add right border to sudoku game
+    if (i % 9 === 0) {
+        document.getElementById('sudoku-element-' + i).style.borderRight = '2px solid';
+    }
+    // Add bottom border to sudoku game
+    if (i > 72) {
+        document.getElementById('sudoku-element-' + i).style.borderBottom = '2px solid';
+    }
+    // Separate game into 9 boxes (thick bottom line)
+    if ((18 < i && i <= 27) || (45 < i && i <= 54)) {
+        document.getElementById('sudoku-element-' + i).style.borderBottom = 'solid';
+    }
 }
