357a3814fc3df5f2019446bf51eabeeb5fd68158
klapoon <35404451+klapoon@users.noreply.github.com>
2021-02-11 18:39:50 +0100

    Change sudoku grid to use rem instead of px for border

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ for (let i = 1; i < 82; i++) {
     sudoku_grid.innerHTML += '<div class="sudoku-element" id="sudoku-element-' + i + '"><input type="text" maxlength="1" minlength="1"></div>'
     // Add right border to sudoku game
     if (i % 9 === 0) {
-        document.getElementById('sudoku-element-' + i).style.borderRight = '2px solid';
+        document.getElementById('sudoku-element-' + i).style.borderRight = '0.2rem solid';
     }
     // Add bottom border to sudoku game
     if (i > 72) {
-        document.getElementById('sudoku-element-' + i).style.borderBottom = '2px solid';
+        document.getElementById('sudoku-element-' + i).style.borderBottom = '0.2rem solid';
     }
     // Separate game into 9 boxes (thick bottom line)
     if ((18 < i && i <= 27) || (45 < i && i <= 54)) {
